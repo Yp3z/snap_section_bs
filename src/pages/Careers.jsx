23b78670d7443ef828e39f9c3ead0565c203371a
Matheo <mt3oyepez@gmail.com>
2026-03-06 11:17:55 -0500

    v1

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -45,6 +45,18 @@ export const Careers = () => {
   </div>
 </div>
 
+  <div class="row align-items-start mt-5">
+    <div class="col">
+      <h1>Built for real life</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit earum, incidunt labore cum deserunt quam doloribus, quaerat quibusdam voluptas laboriosam ratione nam commodi facere assumenda quia expedita! Fuga, numquam quos.  </p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit earum, incidunt labore cum deserunt quam doloribus.</p>
+    </div>
+    <div class="col">
+      <img src="./public/ratitas.webp" alt="" height="350" className="rounded-5" class="border border-5 rounded-5" />
+    </div>
+
+  </div>
+
 <div>
         <div class="p-3 mb-2 bg-secondary text-white text-center mt-5">
             <h1>Ready to cook smarter?</h1>
